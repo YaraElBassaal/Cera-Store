17e15ca0bcf8be8704c2060992adb0523ef146fd
Aladdin-Elshamy <122921299+Aladdin-Elshamy@users.noreply.github.com>
2024-04-23 18:33:41 +0200

    Upload about icons

--- components/About.jsx
+++ components/About.jsx
@@ -13,14 +13,14 @@ export default function About() {
                 <div className="about-info-targets">
                     <div className="about-info-target-one">
                         <div className="about-icon">
-                            <img src="./images/vission-icon.png" alt="" srcset="" />
+                            <img src="./images/vission-icon.png" alt="" />
                         </div>
                         <h3 className="target-one-title">OUR VISION</h3>
                         <p className="target-one-text">At Cera, we envision a world where style meets substance, where every step taken and every accessory carried reflects not just fashion, but also individuality, confidence, and purpose.</p>
                     </div>
                     <div className="about-info-target-two">
                         <div className="about-icon">
-                            <img src="./images/mission-icon.png" alt="" srcset="" />
+                            <img src="./images/mission-icon.png" alt="" />
                         </div>
                         <h3 className="target-two-title">OUR MISSION</h3>
                         <p className="target-two-text">At Cera, our mission is to empower individuals to step into their true selves with confidence and style. We are dedicated to providing a diverse range of high-quality footwear and accessories that not only elevate outfits but also elevate spirits.</p>
